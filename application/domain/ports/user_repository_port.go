2c19d3df37e490863c1cb8e378ea7627ddd8dd5e
Salvador Gonzalez Nolasco <salvador@DellDebian12.tarango>
2024-07-27 00:00:36 -0600

    Port user repository

---
+++ application/domain/ports/user_repository_port.go
@@ -0,0 +1,10 @@
+package ports
+
+import (
+	"github.com/chava.gnolasco/polaris/application/domain/model"
+)
+
+type UserRepositoryPort interface {
+	FindAll() []model.User
+	FindByEmail(email string) *model.User
+}
